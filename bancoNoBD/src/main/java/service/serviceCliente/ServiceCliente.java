502b58270fc8f10162afe47070c79637cd7ac851
Sczuk <hgreasczuk@gmail.com>
2025-10-23 23:44:49 -0300

    Atualizando a service

--- bancoNoBD/src/main/java/service/serviceCliente/ServiceCliente.java
+++ bancoNoBD/src/main/java/service/serviceCliente/ServiceCliente.java
@@ -1,37 +1,48 @@
 package service.serviceCliente;
 
 
-import model.pessoa.Pessoa;
 import model.pessoa.clientes.Cliente;
-
-import java.util.Arrays;
 
 public class ServiceCliente {
 
     public static int getIdade(Cliente cliente){ //pegar a data atual e retornar a idade nao apenas com o ano mas com o mes tbm
-        StringBuilder sb = new StringBuilder();
+
         String data = cliente.getDataDeNascimento();
         char[] dataArray = data.toCharArray();
         char[] anoArray = new char[4];
         int anoNascimento = 0;
+        String anoString = "";
 
         for(int i = 6; i < dataArray.length; i++){
             anoArray[i-6] = dataArray[i];
         }
-
         for(char c : anoArray){
-            sb.append(c);
+            anoString += c;
         }
-
-        String anoString = sb.toString();
 
         try {
             anoNascimento = Integer.parseInt(anoString);
         }catch(NumberFormatException e){
-            throw new NumberFormatException("Ano nao correspondente a um numero");
+            e.printStackTrace();
         }
 
         int idade = 2025 - anoNascimento;
         return idade;
+    }
+
+    public static boolean cadastrarCliente(){
+        return false;
+    }
+
+    public static boolean deletarCliente(){
+        return false;
+    }
+
+    public static Cliente getCliente(){
+        return null;
+    }
+
+    public static boolean updateClienteName(){
+        return false;
     }
 }
--- bancoNoBD/src/main/java/test/test.java
+++ bancoNoBD/src/main/java/test/test.java
@@ -6,11 +6,11 @@ import service.serviceCliente.ServiceCliente;
 
 public class test {
     public static void main(String[] args) {
-        Cliente user = new Cliente("nome","12345678","21/12/2008",1);
+        Cliente user = new Cliente("nome","12345678","21/12/oioi",1);
         Cliente user1 = new Cliente("henrique","12345678","21/12/2010",1);
 
         System.out.println(ServiceCliente.getIdade(user));
-        System.out.println("===============================");
+        System.out.println("=====");
         System.out.println(ServiceCliente.getIdade(user1));
     }
 }
